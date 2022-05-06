166068e4cc5ca9d8261cf43774ce247962f7e8a1
Patrick Quilty <pquilty79@gmail.com>
2022-05-06 09:45:07 -0500

    testing

---
+++ src/main/java/com/example/petadoptionproject/PetAdoptionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.petadoptionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetAdoptionProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petadoptionproject/PetAdoptionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petadoptionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
