1b3e865f1e6eed8c7c9dcc1e7420236e00a141b9
Patrick Quilty <pquilty79@gmail.com>
2022-05-09 15:52:23 -0500

    touch up login

--- src/main/resources/static/js/views/Login.js
+++ src/main/resources/static/js/views/Login.js
@@ -6,15 +6,22 @@ export default function Login(props) {
     <title>Log In</title>
 </head>
 <body>
-<h1>Log In</h1>
 
+<div class="row log-in-row">
+<div class="card log-in-card"> 
 <form id="login-form">
-    <label for="username">Username</label>
+<h1>Log In</h1>
+    <label for="username">Email</label>
     <input id="username" name="username" type="text"/>
+    <br>
     <label for="password">Password</label>
     <input id="password" name="password" type="password"/>
+    <br>
     <input id="login-btn" type="submit" value="Log In"/>
+    <p id="login-response">Login failed. Please try again.</p>  
 </form>
+</div>
+<div>
 </body>
 </html>`;
 
