0dae041dbd5625ad57f5126578ab8bf547dd5025
Patrick Quilty <pquilty79@gmail.com>
2022-05-09 14:34:07 -0500

    registration done

--- src/main/resources/static/js/views/Register.js
+++ src/main/resources/static/js/views/Register.js
@@ -16,10 +16,37 @@ export default function Register(props) {
                 <form id="register-form">
                     <label for="username">Username</label>
                     <input id="username" name="username" type="text"/>
+                    <br>
                     <label for="email">Email</label>
                     <input id="email" name="email" type="email">
+                    <br>
                     <label for="password">Password</label>
                     <input id="password" name="password" type="password"/>
+                    <br>
+                    <label for="firstName">First Name</label>
+                    <input id="firstName" name="firstName" type="text"/>
+                    <br>
+                    <label for="lastName">Last Name</label>
+                    <input id="lastName" name="lastName" type="text"/>
+                    <br>
+                    <label for="organization">Organization</label>
+                    <input id="organization" name="organization" type="text"/>
+                    <br>
+                    <label for="street">Street Address</label>
+                    <input id="street" name="street" type="text"/>
+                    <br>
+                    <label for="city">City</label>
+                    <input id="city" name="city" type="text"/>
+                    <br>
+                    <label for="state">State</label>
+                    <input id="state" name="state" type="text"/>
+                    <br>                      
+                    <label for="zip">Zip Code</label>
+                    <input id="zip" name="zip" type="text"/> 
+                    <br> 
+                    <label for="phone">Phone Number</label>
+                    <input id="phone" name="phone" type="text"/>
+                    <br>                                                                                       
                     <button id="register-btn" type="button">Register</button>
                 </form>
             </body>
@@ -34,7 +61,15 @@ export function RegisterEvent(){
         let newUser = {
             username: $("#username").val(),
             email: $("#email").val(),
-            password: $("#password").val()
+            password: $("#password").val(),
+            firstName: $("#firstName").val(),
+            lastName: $("#lastName").val(),
+            organization: $("#organization").val(),
+            street: $("#street").val(),
+            city: $("#city").val(),
+            state: $("#state").val(),
+            zip: $("#zip").val(),
+            phone: $("#phone").val()
         }
 
         // logs the newly created user to verify
