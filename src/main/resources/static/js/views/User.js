39ae1decdd82aef55c93dfd587e9e03f0e257a8a
Patrick Quilty <pquilty79@gmail.com>
2022-05-09 22:07:27 -0500

    updated nav bar

--- src/main/resources/static/js/views/User.js
+++ src/main/resources/static/js/views/User.js
@@ -2,7 +2,7 @@ import createView from "../createView.js";
 import {getHeaders} from "../auth.js";
 
 export default function UserIndex(props) {
-    console.log("The frontend did it. HER FAULT");
+
 
     return `
         <header>
@@ -10,21 +10,7 @@ export default function UserIndex(props) {
         </header>
         <main>
             <div class="container-fluid">
-                <div id="user-data-container">
-                    <p class="username" data-id="${props.user.id}">Username: ${props.user.username}</p>
-                    <p class="email" data-id="${props.user.id}">Email: ${props.user.email}</p>
-                    <p class="posts" data-id="${props.user.id}">Posts: ${props.user.posts.map(post => `<span>${post.title}</span>
-                        `).join(', ')}</p>
-                </div>
-                <div id="edit-user-container">
-                    <form>
-                        <div class="form-group">
-                            <label for="edit-user-password">Change Password</label>
-                            <input type="password" class="form-control" id="edit-user-password" placeholder="Enter New Password">
-                        </div>
-                        <button type="submit" class="btn btn-primary" id="edit-password-btn" data-id="${props.user.id}">Submit</button>
-                    </form>
-                </div>
+
                 <button type="button" class="btn btn-primary btn-sm" id="new-listing-btn">Create A New Listing</button>
                 <button type="button" class="btn btn-primary btn-sm" id="edit-listing-btn">Edit An Existing Listing</button>
             </div>
