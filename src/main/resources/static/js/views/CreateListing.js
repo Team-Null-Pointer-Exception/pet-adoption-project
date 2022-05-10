5635fe4562368e5bebf233465a3224f0ec92195c
Brice Ernst <brice.ernst1@gmail.com>
2022-05-10 12:28:06 -0500

    createListing form

--- src/main/resources/static/js/views/CreateListing.js
+++ src/main/resources/static/js/views/CreateListing.js
@@ -2,41 +2,81 @@ import createView from "../createView.js";
 import {getHeaders} from "../auth.js";
 
 export default function CreateListing(props) {
-    console.log("The frontend did it. HER FAULT");
     return `
-        <header>
-            <h1>Create Listing</h1>
-        </header>
-        <main>
-            <form>
-                <div class="form-group">
-                    <label for="formGroupExampleInput">Example label</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+    <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8"/>
+                <title>Create A Listing</title>
+            </head>
+            <body>
+            <div class="row create-listing-row">
+            <div class="card create-listing-card"> 
+                <form id="create-listing-form">
+                    <h1>Create A Listing</h1>
+                    <label for="name">Name</label>
+                    <input id="name" name="name" type="text"/>
+                    <br>
+                    <label for="animal">Animal</label>
+                    <input id="animal" name="animal" type="text"/>
+                    <br>
+                    <label for="breed">Breed</label>
+                    <input id="breed" name="breed" type="text"/>
+                    <br>
+                    <label for="color">Color</label>
+                    <input id="color" name="color" type="text"/>
+                    <br>
+                    <label for="age">Age</label>
+                    <input id="age" name="age" type="text"/>
+                    <br>
+                    <label for="sex">Sex</label>
+                    <input id="sex" name="sex" type="text"/>
+                    <br>
+                    <label for="health">Health</label>
+                    <input id="health" name="health" type="text"/>
+                    <br>
+                    <label for="fixed">Fixed</label>
+                    <select class="form-control" id="fixed">
+                      <option>True</option>
+                      <option>False</option>
+                    </select>
+                    <br>
+                    <label for="description">Description</label>
+                    <textarea id="description" name="description" rows="2" placeholder="Pet description"></textarea>
+                    <br>
+                    <label for="summary">Summary</label>
+                    <textarea id="summary" name="summary" rows="3" placeholder="Listing information"></textarea>
+                    <br>                                                                                       
+                    <button id="create-listing-btn" type="button">Submit</button>
+                </form>
                 </div>
-                <div class="form-group">
-                    <label for="formGroupExampleInput2">Another label</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
                 </div>
-                <input id="create-listing-btn" type="submit" value="Submit"/>
-            </form>
-        </main>
+            </body>
+        </html>
     `;
 }
 
-export function CreateListingsEvent(){
+function createListing(){
     $('#create-listing-btn').click(function () {
 
-        let title = $('#add-post-title').val();
-        let content = $('#add-post-content').val();
-        let post = {
-            title,
-            content
+        let newListing = {
+            summary: $("#summary").val(),
+            age: $("#age").val(),
+            animal: $("#animal").val(),
+            breed: $("#breed").val(),
+            color: $("#color").val(),
+            description: $("#description").val(),
+            fixed: $("#fixed").val(),
+            health: $("#health").val(),
+            name: $("#name").val(),
+            sex: $("#sex").val(),
+            images: imageArray
         }
 
         let request = {
             method: "POST",
             headers: getHeaders(),
-            body: JSON.stringify(post)
+            body: JSON.stringify(newListing)
         }
 
         fetch("http://localhost:8080/api/listings/create", request)
@@ -48,4 +88,8 @@ export function CreateListingsEvent(){
             createView("/users");
         });
     })
+}
+
+export function CreateListingsEvent(){
+    createListing();
 }
