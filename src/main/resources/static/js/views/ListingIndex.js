cb4da755026e60f5c6db3bc6fcba33454e9fbd5f
Patrick Quilty <pquilty79@gmail.com>
2022-05-12 09:40:32 -0500

    added call function

--- src/main/resources/static/js/views/ListingIndex.js
+++ src/main/resources/static/js/views/ListingIndex.js
@@ -72,26 +72,7 @@ export default function ListingIndex(props) {
                     <div class="container px-4 px-lg-5 mt-5">
                         <div id="listing-cards"
                              class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                            ${activeListings.map(listing =>
-        `<div class="col mb-5">
-                                <div class="card h-100">
-                                    <!-- Pet image-->
-                                    <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                                    <!-- Pet details-->
-                                    <div class="card-body p-4 bg-light">
-                                        <div class="text-center">
-                                            <!-- Pet name-->
-                                            <h5 class="fw-bolder">${listing.name}</h5>
-                                            <!-- Breed-->
-                                            ${listing.breed}
-                                        </div>
-                                    </div>
-                                    <!-- View details-->
-                                    <div class="card-footer p-4 pt-0 border-top-0 bg-light">
-                                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
-                                    </div>
-                                </div>
-                            </div>`).join('')}
+                            ${populateCards(activeListings)}
                         </div>
                     </div>
                 </section>
