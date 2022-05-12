abe8e233670b93618a6c1906f0b648dd2343abcf
Justin Sixsmith <justinsixsmith@gmail.com>
2022-05-12 17:02:45 -0500

    reworks the listings page

--- src/main/resources/static/js/views/ListingIndex.js
+++ src/main/resources/static/js/views/ListingIndex.js
@@ -1,7 +1,7 @@
 import createView from "../createView.js";
 import {getHeaders, getUserRole} from "../auth.js";
 
-let allListings;
+let allListings, activeListings, listingStatus, animalType, gender, distance;
 
 export default function ListingIndex(props) {
     console.log(getUserRole());
@@ -11,7 +11,7 @@ export default function ListingIndex(props) {
             listing.breed = listing.animal;
         }
     })
-    let activeListings = allListings.filter(listing => listing.status === "ACTIVE");
+    activeListings = allListings.filter(listing => listing.status === "ACTIVE");
 
     //language=HTML
     return `
@@ -29,40 +29,31 @@ export default function ListingIndex(props) {
             </p>
 
             <div class="py-5">
-                ${adminView()}
-                <div class="btn-group">
-                    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">Animal Type
-                    </button>
-                    <div class="dropdown-menu">
-                        <a id="all-animals" class="dropdown-item">All</a>
-                        <div class="dropdown-divider"></div>
-                        <a id="dogs" class="dropdown-item">Dogs</a>
-                        <a id="cats" class="dropdown-item">Cats</a>
-                        <a id="other" class="dropdown-item">Other</a>
-                    </div>
-                </div><!-- /btn-group -->
-                <div class="btn-group">
-                    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">Male / Female
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">All</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Male</a>
-                        <a class="dropdown-item" href="#">Female</a>
-                    </div>
-                </div><!-- /btn-group -->
-                <div class="btn-group">
-                    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">Distance from Me
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item">Within 10 Miles</a>
-                        <a class="dropdown-item">Within 50 Miles</a>
-                        <a class="dropdown-item">Any Distance</a>
-                    </div>
-                </div><!-- /btn-group -->
+                ${adminMenu()}
+                <div class="btn-group m-2">
+                    <select id="animal-type" class="form-select btn-secondary" aria-label="Animal type">
+                        <option class="default">All</option>
+                        <option>Dogs</option>
+                        <option>Cats</option>
+                        <option>Other</option>
+                    </select>
+                </div>
+
+                <div class="btn-group m-2">
+                    <select id="gender" class="form-select btn-secondary" aria-label="Gender">
+                        <option class="default">Male or Female</option>
+                        <option>Male</option>
+                        <option>Female</option>
+                    </select>
+                </div>
+
+                <div class="btn-group m-2">
+                    <select id="distance" class="form-select btn-secondary" aria-label="Distance">
+                        <option class="default">Within 15 Miles</option>
+                        <option>Within 50 Miles</option>
+                        <option>Any Distance</option>
+                    </select>
+                </div>
             </div>
         </div>
 
@@ -82,49 +73,90 @@ export default function ListingIndex(props) {
         <footer class="py-5 bg-dark">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; TakeMyPig.com 2022</p></div>
     `;
+}
 
+function adminMenu() {
+    if (getUserRole()) {
+        return "";
+    } else {
+        //language=HTML
+        return `
+            <div class="btn-group m-2">
+                <select id="listing-status" class="form-select btn-secondary" aria-label="Listing status">
+                    <option class="default">Status (All)</option>
+                    <option>Active</option>
+                    <option>Pending</option>
+                    <option>Expired</option>
+                    <option>Closed</option>
+                </select>
+            </div>
+        `;
+    }
 }
 
 export function ListingsEvent() {
-    selectAllListings();
-    selectDogs();
-    selectCats();
-    selectOther();
+    grabSelections();
+    filterSelections();
 }
 
-function selectAllListings() {
-    $("#all-animals").click(function () {
-        const activeListings = allListings.filter(listing => listing.status === "ACTIVE");
-        return $("#listing-cards").html(populateCards(activeListings));
-    });
+function grabSelections() {
+    listingStatus = $("#listing-status").val().toLowerCase();
+    animalType = $("#animal-type").val().toLowerCase();
+    gender = $("#gender").val().toLowerCase();
+    distance = $("#distance").val().toLowerCase();
 }
 
-function selectDogs() {
-    const dogs = allListings.filter(listing => listing.animal.toLowerCase() === "dog" && listing.status === "ACTIVE");
-    $("#dogs").click(function () {
-        return $("#listing-cards").html(populateCards(dogs));
+function filterSelections() {
+
+    $("#listing-status, #animal-type, #gender, #distance").change(function () {
+        grabSelections();
+        console.log(listingStatus);
+        console.log(animalType);
+        console.log(gender);
+        console.log(distance);
+
+        let filteredListings;
+        if (listingStatus === "active") {
+            filteredListings = activeListings;
+        } else if (listingStatus === "pending") {
+            filteredListings = allListings.filter(listing => listing.status === "PENDING");
+        } else if (listingStatus === "expired") {
+            filteredListings = allListings.filter(listing => listing.status === "EXPIRED");
+        } else if (listingStatus === "closed") {
+            filteredListings = allListings.filter(listing => listing.status === "CLOSED");
+        } else {
+            filteredListings = allListings;
+        }
+
+        console.log(filteredListings);
+
+        if (animalType === "dogs") {
+            filteredListings = filteredListings.filter(listing => listing.animal === "dog");
+        } else if (animalType === "cats") {
+            filteredListings = filteredListings.filter(listing => listing.animal === "cat");
+        } else if (animalType === "other") {
+            filteredListings = filteredListings.filter(listing => listing.animal !== "dog" && listing.animal !== "cat");
+        }
+
+        console.log(filteredListings)
+
+        if (gender === "male") {
+            filteredListings = filteredListings.filter(listing => listing.sex === "MALE");
+        } else if (gender === "female") {
+            filteredListings = filteredListings.filter(listing => listing.sex === "FEMALE");
+        }
+
+        console.log(filteredListings)
+
+        return $("#listing-cards").html(populateCards(filteredListings));
     })
 }
 
-function selectCats() {
-    const cats = allListings.filter(listing => listing.animal.toLowerCase() === "cat" && listing.status === "ACTIVE");
-    $("#cats").click(function () {
-        return $("#listing-cards").html(populateCards(cats));
-    })
-}
 
-function selectOther() {
-    const other = allListings.filter(listing => (listing.animal.toLowerCase() !== "dog" && listing.animal.toLowerCase() !== "cat") && (listing.status === "ACTIVE"));
-    $("#other").click(function () {
-        return $("#listing-cards").html(populateCards(other));
-    })
-}
-
-
-function populateCards(animalListings) {
+function populateCards(filteredListings) {
     //language=HTML
     return `
-        ${animalListings.map(listing =>
+        ${filteredListings.map(listing =>
                 `<div class="col mb-5">
                         <div class="card h-100">
                             <!-- Pet image-->
@@ -146,26 +178,4 @@ function populateCards(animalListings) {
                     </div>`).join('')}`
 }
 
-function adminView() {
-    if (!getUserRole()) {
-        return "";
-    } else {
-        //language=HTML
-        return `
-            <div class="btn-group">
-                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">Listing Status
-                </button>
-                <div class="dropdown-menu">
-                    <a id="all-listings" class="dropdown-item">All</a>
-                    <div class="dropdown-divider"></div>
-                    <a id="pending" class="dropdown-item">Pending</a>
-                    <a id="active" class="dropdown-item">Active</a>
-                    <a id="closed" class="dropdown-item">Closed</a>
-                    <a id="expired" class="dropdown-item">Expired</a>
-                </div>
-            </div>
-        `;
-    }
 
-}
