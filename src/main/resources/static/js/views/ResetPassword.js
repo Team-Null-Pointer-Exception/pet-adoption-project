5fe23d303c773be7ec7bd81e7b080a082348be69
Patrick Quilty <pquilty79@gmail.com>
2022-05-19 17:09:21 -0500

    touch up reset

--- src/main/resources/static/js/views/ResetPassword.js
+++ src/main/resources/static/js/views/ResetPassword.js
@@ -4,27 +4,22 @@ import {token} from "../router.js";
 
 
 export default function ResetPassword() {
-    return `
-<div class="row reset-row">
-<div class="card reset-card"> 
-
-<form id="reset-form">
-<h1 class="text-white">Forgot Your Password?</h1>
-            <p>
-                <input type="password" name="password" id="new-password" class="form-control"
-                       placeholder="Enter your new password" required autofocus />
-            </p>
-            <p>
-                <input type="password" class="form-control" id="confirm-new-password" placeholder="Confirm your new password"/>
-            </p>
-            <p class="text-center">
-                <input id="reset-btn" class="allow" type="button" value="Reset Password"/>
-            </p>
-            <p id="password-response"></p>
-</form>
-</div>
-</div>
-    `
+        return`      
+                    <div class="row reset-row">
+                    <div class="card reset-card"> 
+                    <form id="reset-form">
+                    <h1 class="text-white">Reset Your Password</h1>
+                        <label for="new-password">Enter new password:</label>
+                        <input id="new-password" name="new-password" type="password"/>
+                        <br>
+                        <label for="confirm-new-password">Enter new password:</label>
+                        <input id="confirm-new-password" name="confirm-new-password" type="password"/>
+                        <br>
+                        <input id="reset-btn" class="allow" type="button" value="Save"/>
+                        <p id="password-response">Passwords do not match</p>  
+                    </form>
+                    </div>
+                    </div>`;
 }
 
 
