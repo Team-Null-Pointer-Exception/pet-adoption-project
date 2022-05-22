10404b528a026912fad96d8fe8a73d1d687cc530
Patrick Quilty <pquilty79@gmail.com>
2022-05-22 12:59:01 -0500

    login page formatting

--- src/main/resources/static/js/views/Login.js
+++ src/main/resources/static/js/views/Login.js
@@ -14,8 +14,9 @@ export default function Login(props) {
             <input id="password" name="password" type="password"/>
             <br>
             <input id="login-btn" type="submit" class="mt-2" value="Log In"/>
-            <p id="login-response">Login failed. Please try again.</p>  
             <a href="/forgot" data-link>Forgot your password?</a>
+            <br>
+            <p id="login-response">Login failed. Please try again.</p>
         </form>
     </div>
 </div>
