8c3199e5ec5806da7d74b0839b93b40795d82531
Patrick Quilty <pquilty79@gmail.com>
2022-05-22 12:56:08 -0500

    fixed login page for mobile

--- src/main/resources/static/js/views/Login.js
+++ src/main/resources/static/js/views/Login.js
@@ -1,21 +1,23 @@
 export default function Login(props) {
     return `
-
+<div class="container-fluid">
 <div class="row log-in-row">
-<div class="card log-in-card"> 
-<form id="login-form">
-<h1 class="text-white">Log In</h1>
-    <label for="username">Email</label>
-    <input id="username" name="username" type="text"/>
-    <br>
-    <label for="password">Password</label>
-    <input id="password" name="password" type="password"/>
-    <br>
-    <input id="login-btn" type="submit" value="Log In"/>
-    <p id="login-response">Login failed. Please try again.</p>  
-    
-    <a href="/forgot" data-link>Forgot your password?</a>
-</form>
+    <div class="card log-in-card"> 
+        <form id="login-form">
+            <h1 class="text-white">Log In</h1>
+            <label for="username">Email</label>
+            <br>
+            <input id="username" name="username" type="text"/>
+            <br>
+            <label for="password">Password</label>
+            <br>
+            <input id="password" name="password" type="password"/>
+            <br>
+            <input id="login-btn" type="submit" class="mt-2" value="Log In"/>
+            <p id="login-response">Login failed. Please try again.</p>  
+            <a href="/forgot" data-link>Forgot your password?</a>
+        </form>
+    </div>
 </div>
 </div>
 `;
